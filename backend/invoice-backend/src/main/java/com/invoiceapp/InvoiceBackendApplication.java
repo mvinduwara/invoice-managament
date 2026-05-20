4286b76e9d833ec35e68cbfad0a7dd9524849f21
mvinduwara <mvinduwara@gmail.com>
2026-05-20 17:46:59 +0530

    new commit

---
+++ backend/invoice-backend/src/main/java/com/invoiceapp/InvoiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.invoiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceBackendApplication.class, args);
+    }
+
+}
